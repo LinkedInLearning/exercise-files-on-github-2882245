de4c73970cc9da04d1c8366cb1d8bc1c812d03de
Bhaskar <38725403+bskr2020@users.noreply.github.com>
2024-02-18 15:59:05 +0530

    second commit

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-#variable "instance_type" {
-#  description = "Type of EC2 instance to provision"
-#  default     = "t2.micro"
-#}
+variable "instance_type" {
+  description = "Type of EC2 instance to provision"
+  default     = "t2.micro"
+}
