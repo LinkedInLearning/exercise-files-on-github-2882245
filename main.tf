0b38057eb6a25f19510ae24b5a4b4ba9113b72e3
Bhaskar <38725403+bskr2020@users.noreply.github.com>
2024-02-18 16:07:46 +0530

    test commit

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ data "aws_ami" "app_ami" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
+  #ok
   tags = {
     Name = "HelloWorld"
   }
